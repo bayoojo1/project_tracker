a2128f883ed8ef4f2d89464a5569538b55e35489
Ojo Adebayo <bayoojo1@yahoo.com>
2020-07-23 17:45:49 +0100

    Update connection.php

--- inc/connection.php
+++ inc/connection.php
@@ -1,6 +1,7 @@
 <?php
 try {
-    $db_connect = new PDO('mysql:host=localhost;dbname=project_tracker', 'root', 'wifi1234');
+    //$db_connect = new PDO('mysql:host=localhost;dbname=project_tracker', 'root', 'wifi1234');
+    $db_connect = new PDO("sqlite:".__DIR__."/database.db");
     $db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo $e->getMessage()."<br>";
